80a3178bc0873beb273b972fdb0f1b5559ee14f2
Rachit Aarsh <rachit.aarsh@hotwax.co>
2026-06-25 16:54:46 +0530

    webhooks for product update will be saved in prisma

--- demo-app-cli/app/routes/webhooks.products.update.tsx
+++ demo-app-cli/app/routes/webhooks.products.update.tsx
@@ -1,5 +1,6 @@
 import type { ActionFunctionArgs } from "react-router";
 import { authenticate } from "../shopify.server";
+import db from "../db.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   // 1. Authenticate the webhook to make sure it actually came from Shopify.
@@ -13,6 +14,15 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   console.log(`Store: ${shop}`);
   console.log(`Product Updated: ${payload.title}`);
   console.log(`================================\n`);
+
+  // 4. Data saved in DB
+  await db.productUpdateLog.create({
+    data: {
+      shop: shop,
+      productTitle: String(payload.title),
+      updatedData: JSON.stringify(payload)
+    }
+  });
 
   // 3. You MUST return an empty Response to tell Shopify "Message Received".
   // If you don't, Shopify will think your server is down and keep retrying.
